62e0cc62209e2ee939e2dc560f3e051c277e30a9
ahmed mohamed <ahmed.mohamed@afaqy.com>
2022-02-14 18:05:27 +0200

    update content

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          hello circle ci
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         thank you for watching
         </a>
       </header>
     </div>
